daa81dd67238b584304d3f4166a43f96945ee63d
florian lequere <florian.lequere@gmail.com>
2022-05-19 08:15:21 +0200

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'creating flats'
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Wall Street',
+  address: 'Boulevard Voltaire',
+  description: "La colloc des BG et d'un gros Micha",
+  price_per_night: 60,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Appart de bobos',
+  address: 'Rue Notre-Dame de Lorette',
+  description: "1er appart du Love",
+  price_per_night: 55,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Appart de C++',
+  address: 'Rue de Clichy',
+  description: 'Vue sur jardin, 1er achat',
+  price_per_night: 80,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Maison bordelaise',
+  address: 'Rue Odilon Redon',
+  description: 'Maison déco avec jardin',
+  price_per_night: 200,
+  number_of_guests: 3
+)
+
+puts 'flats created'
